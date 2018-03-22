ec25b6b31a1ab7457191017aa17c16aa82bcf0bd
ayatsau <ayatsau@cisco.com>
2018-03-23 00:38:20 +0100

    Transport abstraction added.

--- src/test/java/by/mrj/LocalMessengerConfig.java
+++ src/test/java/by/mrj/LocalMessengerConfig.java
@@ -4,11 +4,14 @@ import by.mrj.crypto.util.CryptoUtils;
 import by.mrj.crypto.util.EncodingUtils;
 import by.mrj.messaging.network.DiscoveryService;
 import by.mrj.messaging.network.domain.Registration;
+import by.mrj.messaging.network.transport.SimpleSocketTransport;
+import by.mrj.messaging.network.transport.Transport;
 import by.mrj.messenger.MessengerConfig;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -30,6 +33,9 @@ public class LocalMessengerConfig {
     @Configuration
     public static class TestConfig {
 
+        @Value("${app.listener.port}")
+        private int port;
+
         @Bean
         public DiscoveryService discoveryService() {
             DiscoveryService mock = mock(DiscoveryService.class);
@@ -42,6 +48,11 @@ public class LocalMessengerConfig {
                     .build();
             when(mock.getNodeData(anyString(), any())).thenReturn(registration);
             return mock;
+        }
+
+        @Bean
+        public Transport transport() {
+            return new SimpleSocketTransport(port);
         }
     }
 
